f970a3a12b6f844361f9e3dca8c8f98f5c36bdac
hindboussaa <116459428+hindboussaa@users.noreply.github.com>
2026-05-15 11:58:14 +0100

    Add success and cancel pages

--- server/server.js
+++ server/server.js
@@ -12,11 +12,10 @@ app.use(cors());
 app.use(express.json());
 
 // Serve frontend
-app.use(express.static(path.join(__dirname, "../")));
+app.use(express.static(path.resolve(__dirname, "..")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../index.html"));
-});
+res.sendFile(path.resolve(__dirname, "..", "index.html"));});
 
 app.post("/create-checkout-session", async (req, res) => {
   try {
