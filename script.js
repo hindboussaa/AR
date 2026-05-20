2fe2264df5dcde81b55d60badcd811f743f0c6a8
hindboussaa <116459428+hindboussaa@users.noreply.github.com>
2026-05-20 11:15:29 +0100

    mn

--- script.js
+++ script.js
@@ -492,3 +492,18 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+
+
+
+
+window.addEventListener("scroll", () => {
+  document.querySelectorAll(".policy-section").forEach(section => {
+    const rect = section.getBoundingClientRect();
+
+    if (rect.top < window.innerHeight * 0.6 && rect.bottom > 200) {
+      section.classList.add("active");
+    } else {
+      section.classList.remove("active");
+    }
+  });
+});
