81ec6f69b6eaca57e2e8513a2c49478501e9c720
hindboussaa <116459428+hindboussaa@users.noreply.github.com>
2026-05-20 12:43:16 +0100

    cx

--- server/server.js
+++ server/server.js
@@ -15,6 +15,11 @@ app.use(express.json());
 // Serve frontend
 app.use(express.static(path.join(__dirname, "..", "public")));
 
+
+app.get("/favicon.ico", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "favicon.ico"));
+});
+
 app.get("/", (req, res) => {
 res.sendFile(path.resolve(__dirname, "..", "index.html"));});
 
